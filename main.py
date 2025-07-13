9dd7480df7fc2e1eb3399cec5724a6d3513a5e01
Luca Di Felice <ldifelice06@gmail.com>
2025-07-13 19:37:33 -0400

    Refactor mood logging feature and add goals tab in Streamlit application

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import streamlit as st
 import pandas as pd
+from datetime import date
 
-mood_tab, coach_tab = st.tabs(["Mood", "BrightSide coach"])
-finish_button = st.columns([1])
+mood_tab, coach_tab, goals_tab = st.tabs(["Mood", "BrightSide coach", "Goals"])
 
-with mood_tab:
-    st.header("Log your mood")
-    feeling_input = st.text_input("How are you feeling right now?")
-    mood_input = st.text_input("How did you feel overall today")
-    with finish_button[0]:
-        if st.button("Finish", use_container_width=True):
-            st.write("Finish button clicked")
+def main():
+    with mood_tab:
+        today_date = date.today()
+        st.header(today_date)
+        feeling_input = st.text_input("How are you feeling right now?")
+        mood_input = st.text_input("How did you feel overall today")
+        finish_column = st.columns([1])
+        with finish_column[0]:
+            if st.button("Finish", use_container_width=True):
+                st.write("Data sent to ChatGPT")
+
+    with coach_tab:
+        st.write("hello")
+
+main()
