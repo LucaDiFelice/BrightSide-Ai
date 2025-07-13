c1aad80c1e8aa4b06fada18db32fec11aa3f7c86
Luca Di Felice <ldifelice06@gmail.com>
2025-07-12 20:05:59 -0400

    Implement mood logging feature in Streamlit application

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.write("hello world")
-st.write(pd.DataFrame({
-    "first column" : [1,2,3,4],
-    "second column" : [10,20,30,40]
-}))
+mood_tab, coach_tab = st.tabs(["Mood", "BrightSide coach"])
+finish_button = st.columns([1])
+
+with mood_tab:
+    st.header("Log your mood")
+    feeling_input = st.text_input("How are you feeling right now?")
+    mood_input = st.text_input("How did you feel overall today")
+    with finish_button[0]:
+        if st.button("Finish", use_container_width=True):
+            st.write("Finish button clicked")
