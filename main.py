d90edb11808db143a71f349c9c6378855923c606
Luca Di Felice <ldifelice06@gmail.com>
2025-07-19 12:03:19 -0400

    Add mood input handling and integrate ChatGPT response in Streamlit application

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
+import time
+
+from ollama import chat
+from ollama import ChatResponse
 
 mood_tab, coach_tab, goals_tab = st.tabs(["Mood", "BrightSide coach", "Goals"])
 
 def main():
+    feeling_input = None
+    mood_input = None
     with mood_tab:
         today_date = date.today()
         st.header(today_date)
@@ -14,8 +20,13 @@ def main():
         with finish_column[0]:
             if st.button("Finish", use_container_width=True):
                 st.write("Data sent to ChatGPT")
-
-    with coach_tab:
-        st.write("hello")
+                response: ChatResponse = chat(model="deepseek-r1:1.5b", messages=[
+                    {
+                        "role" : "user",
+                        "content" : feeling_input
+                    },
+                ])
+                time.sleep(10)
+                st.write(response["message"]["content"])
 
 main()
